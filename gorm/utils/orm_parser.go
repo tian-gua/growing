04e21551b6af15bc127473029762a9d94f67a8c5
叶浩 <77729052@qq.com>
2016-06-26 17:53:50 +0800

    将反射相关的函数放到 reflect_util文件里

--- gorm/utils/orm_parser.go
+++ gorm/utils/orm_parser.go
@@ -1,46 +1,11 @@
 package utils
 
 import (
-	"reflect"
-	"strconv"
 	"regexp"
 	"strings"
 )
 
-//将接受的反射的值 转为 字符换类型
-func Parse(v reflect.Value) string {
 
-	var result string
-	//根据值得类型转换字符串
-	switch v.Kind() {
-	case reflect.String:
-		result = "'" + v.String() + "'"
-	case reflect.Int:
-		result = strconv.FormatInt(v.Int(), 10)
-	}
-	return result
-
-}
-
-
-//判断是否是零值
-func IsZero(v reflect.Value) bool {
-	//获得值得类型
-	kind := v.Kind()
-	switch kind {
-	case reflect.String:
-		if "" == v.String() {
-			return true
-		}
-		return false
-	case reflect.Int:
-		if 0 == v.Int() {
-			return true
-		}
-		return false
-	}
-	return false
-}
 
 
 //将字符串转为驼峰命名规则,并且首字母大写
@@ -100,3 +65,4 @@ func UnCamelCase(str string) (string, error) {
 	return strings.TrimLeft(str, "_"), nil
 
 }
+
---
+++ gorm/utils/reflect_util.go
@@ -0,0 +1,42 @@
+package utils
+
+import (
+	"reflect"
+	"strconv"
+)
+
+//判断是否是零值
+func IsZero(v reflect.Value) bool {
+	//获得值得类型
+	kind := v.Kind()
+	switch kind {
+	case reflect.String:
+		if "" == v.String() {
+			return true
+		}
+		return false
+	case reflect.Int:
+		if 0 == v.Int() {
+			return true
+		}
+		return false
+	}
+	return false
+}
+
+
+//将接受的反射的值 转为 字符换类型
+func Parse(v reflect.Value) string {
+
+	var result string
+	//根据值得类型转换字符串
+	switch v.Kind() {
+	case reflect.String:
+		result = "'" + v.String() + "'"
+	case reflect.Int:
+		result = strconv.FormatInt(v.Int(), 10)
+	}
+	return result
+
+}
+
