0a9d6836feb315cb296038daf41ae0e3867ca775
叶浩 <77729052@qq.com>
2016-07-06 13:12:26 +0800

    添加了一个比较棒的功能， 在http 处理器 里添加一个结构体参数， 结构体的值会自动从 表单或者url后面的 参数中 注入 ，根据属性名字，要求大写开头的属性

--- grouter/controller.go
+++ grouter/controller.go
@@ -5,9 +5,11 @@ import (
 	"fmt"
 	"regexp"
 	"strings"
+	"reflect"
+	"strconv"
 )
 
-type handler func(rw http.ResponseWriter, req *http.Request)
+type handler interface{}
 
 
 //定义控制器的接口,所有接口必须实现 Get Post 方法
@@ -32,7 +34,9 @@ func (c *controller) addPost(pattern string, h handler) {
 func (c *controller) get(pattern string, rw http.ResponseWriter, req *http.Request) {
 
 	if h, ok := c.getHandlers[TrimParameter(pattern)]; ok {
-		h(rw, req)
+		if checkHandler(h) {
+			do(rw, req, h)
+		}
 	} else {
 		fmt.Println("未找到[" + pattern + "]对应的GET处理器!")
 		rw.WriteHeader(http.StatusNotFound)
@@ -42,7 +46,9 @@ func (c *controller) get(pattern string, rw http.ResponseWriter, req *http.Reque
 //请求post方法
 func (c *controller)  post(pattern string, rw http.ResponseWriter, req *http.Request) {
 	if h, ok := c.postHandlers[TrimParameter(pattern)]; ok {
-		h(rw, req)
+		if checkHandler(h) {
+			do(rw, req, h)
+		}
 	} else {
 		fmt.Println("未找到[" + pattern + "]对应的POST处理器!")
 		rw.WriteHeader(http.StatusNotFound)
@@ -56,4 +62,57 @@ func TrimParameter(url string) string {
 		return url
 	}
 	return strings.TrimRight(url, reg.FindString(url))
+}
+
+
+//检查处理器是不是func
+func checkHandler(h handler) bool {
+	k := reflect.TypeOf(h).Kind()
+	if reflect.Func == k {
+		return true
+	}
+	return false
+}
+
+func do(rw http.ResponseWriter, req *http.Request, h handler) {
+	funcT := reflect.TypeOf(h)
+	funcV := reflect.ValueOf(h)
+	numin := funcT.NumIn()
+	vs := new([]reflect.Value)
+	for i := 0; i < numin; i++ {
+		if funcT.In(i).Kind() == reflect.Ptr {
+			if "Request" == funcT.In(i).Elem().Name() {
+				*vs = append(*vs, reflect.ValueOf(req))
+			}
+		} else {
+			if "ResponseWriter" == funcT.In(i).Name() {
+				*vs = append(*vs, reflect.ValueOf(rw))
+			} else if reflect.Struct == funcT.In(i).Kind() {
+				v := reflect.New(funcT.In(i)).Elem()
+				t := reflect.TypeOf(v.Interface())
+				//遍历属性,并赋值
+				//值从表单里取,根据属性名字对应
+				for j := 0; j < t.NumField(); j++ {
+					sf := t.Field(j)
+					sfv := v.Field(j)
+					//注入不同类型的值
+					switch sf.Type.Kind() {
+					case reflect.String:
+						sfv.SetString(req.FormValue(sf.Name))
+					case reflect.Int:
+						conv, err := strconv.Atoi(req.FormValue(sf.Name))
+						if err != nil {
+							fmt.Println(err)
+						} else {
+							sfv.SetInt(int64(conv))
+						}
+					default:
+						fmt.Println("注入参数失败!")
+					}
+				}
+				*vs = append(*vs, v)
+			}
+		}
+	}
+	funcV.Call(*vs)
 }
