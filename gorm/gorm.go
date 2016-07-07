6d832f16e9a2a2efab186c33a6bd4f0c9211dc39
叶浩 <77729052@qq.com>
2016-07-07 10:35:30 +0800

    完善自定义sql查询， 根据target的类型 进行不同的映射  target类型可以为 结构体切片（例如：表的所有记录）， 基础类型切片（例如：表的所有id或者name）， 基础类型（某一条记录的id或者name）

--- gorm/gorm.go
+++ gorm/gorm.go
@@ -173,14 +173,11 @@ func CustomQuery(sqlStr string, target interface{}) error {
 	//获得target的反射信息
 	targetV := reflect.Indirect(reflect.ValueOf(target))
 
-	elementType := getEmptySliceValue(targetV).Type()
-
 	//查询
 	rows, err := gdb.Query(sqlStr)
 	if err != nil {
 		return err
 	}
-
 	//获得所有列
 	columns, err := rows.Columns()
 	if err != nil {
@@ -195,28 +192,55 @@ func CustomQuery(sqlStr string, target interface{}) error {
 	for i := range values {
 		scans[i] = &values[i]
 	}
-	var index int = 0
-	//遍历所有记录
-	for rows.Next() {
+	//如果传过来的是一个切片
+	if reflect.Slice == targetV.Type().Kind() {
+		elementType := getEmptySliceValue(targetV).Type()
+		var index int = 0
+		//遍历所有记录
+		for rows.Next() {
+			err := rows.Scan(scans...)
+			if err != nil {
+				return err
+			}
+
+			//根据反射来新建一个和记录对应的对象
+			var newV = reflect.New(elementType).Elem()
+			//如果切片类型为结构体
+			if reflect.Struct == elementType.Kind() {
+				for i := 0; i < colNum; i++ {
+					colName := columns[i]
+					setValue(newV.FieldByName(gutils.ToCamelCase(colName)), values[i])
+				}
+			} else {
+				//如果是 基础类型 则直接赋值
+				setValue(newV, values[0])
+			}
+			targetV = reflect.Append(targetV, newV)
+			index++
+		}
+		//更新target的值
+		reflect.ValueOf(target).Elem().Set(targetV.Slice(0, index))
+
+	} else {
+		//target为单条记录
+		rows.Next()
 		err := rows.Scan(scans...)
 		if err != nil {
 			return err
 		}
-		//根据反射来新建一个和记录对应的对象
-		var newV = reflect.New(elementType).Elem()
-		for i := 0; i < colNum; i++ {
-			colName := columns[i]
-			setValue(newV.FieldByName(gutils.ToCamelCase(colName)), values[i])
+		//如果target的为单个结构体
+		if reflect.Struct == targetV.Type().Kind() {
+			for i := 0; i < colNum; i++ {
+				colName := columns[i]
+				setValue(targetV.FieldByName(gutils.ToCamelCase(colName)), values[i])
+			}
+		} else {
+			setValue(targetV, values[0])
 		}
-		targetV = reflect.Append(targetV, newV)
-		index++
 	}
-	//更新target的值
-	reflect.ValueOf(target).Elem().Set(targetV.Slice(0, index))
 	return nil
 
 }
-
 
 //获得空切片元素的类型
 func getEmptySliceValue(slice reflect.Value) reflect.Value {
