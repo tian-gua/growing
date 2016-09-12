9f99cbd5e5dd4ed39b7c786d9264c1f4b4d28117
叶浩 <77729052@qq.com>
2016-09-12 23:33:04 +0800

    如果读取不到配置文件，session闲置时间设置成默认30分钟

--- session/session_config.go
+++ session/session_config.go
@@ -13,18 +13,21 @@ var Session_time time.Duration
 func init() {
 	//读取配置文件
 	//在gopath下查找
-	properties := gutils.GetIniProperties("./growing.ini")
-	if m, ok := properties["session"]; ok {
-		if v, ok := m["session_time"]; ok {
-			m, err := strconv.Atoi(v)
-			if err != nil {
-				panic(err)
+	properties, err := gutils.GetIniProperties("../growing.ini")
+	if err == nil {
+		if m, ok := properties["session"]; ok {
+			if v, ok := m["session_time"]; ok {
+				m, err := strconv.Atoi(v)
+				if err != nil {
+					panic(err)
+				}
+				Session_time = time.Duration(m) * time.Minute
+				fmt.Printf("session闲置时间为:%s\n", Session_time)
 			}
-			Session_time = time.Duration(m) * time.Minute
-			fmt.Printf("session闲置时间为:%s\n", Session_time)
-		}else {
-			Session_time = 30 * time.Minute
-			fmt.Printf("session闲置时间为默认时间:%s\n", Session_time)
+			return
 		}
 	}
+	Session_time = 30 * time.Minute
+	fmt.Printf("session闲置时间为默认时间:%s\n", Session_time)
+
 }
