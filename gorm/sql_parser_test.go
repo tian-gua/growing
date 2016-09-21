d6af47365b8da0defc33eee24d5d0891d850e10e
叶浩 <77729052@qq.com>
2016-09-21 21:24:46 +0800

    .

--- gorm/sql_parser_test.go
+++ gorm/sql_parser_test.go
@@ -2,7 +2,6 @@ package gorm
 
 import (
 	_ "github.com/go-sql-driver/mysql"
-	"fmt"
 	"testing"
 )
 
@@ -14,10 +13,10 @@ type userAgent struct {
 }
 
 func Test_ParseSql(t *testing.T) {
-	fmt.Println(ParseDeleteByPrimaryKeySql(&userAgent{Id:1, Age:2}))
-	fmt.Println(ParseQueryAllSql(&userAgent{}))
-	fmt.Println(ParseQuerySql(&userAgent{Id:1, Name:"aaa", Age:22, Sex:1}))
-	fmt.Println(ParseSaveSql(&userAgent{Id:1, Name:"aaa", Sex:1}))
-	fmt.Println(ParseSaveSql(&userAgent{Name:"aaa", Age:22}))
-	fmt.Println(ParseSaveSql(&userAgent{Id:1, Name:"阿斯蒂芬", Age:44, Sex:1}))
+	t.Log(ParseDeleteByPrimaryKeySql(&userAgent{Id:1, Age:2}))
+	t.Log(ParseQueryAllSql(&userAgent{}))
+	t.Log(ParseQuerySql(&userAgent{Id:1, Name:"aaa", Age:22, Sex:1}))
+	t.Log(ParseSaveSql(&userAgent{Id:1, Name:"aaa", Sex:1}))
+	t.Log(ParseSaveSql(&userAgent{Name:"aaa", Age:22}))
+	t.Log(ParseSaveSql(&userAgent{Id:1, Name:"阿斯蒂芬", Age:44, Sex:1}))
 }
