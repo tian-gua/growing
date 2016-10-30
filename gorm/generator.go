ef3dfe44da4cb712af30e7098869ef878ca6c772
叶浩 <77729052@qq.com>
2016-10-30 15:35:28 +0800

    .

--- gorm/generator.go
+++ gorm/generator.go
@@ -48,7 +48,7 @@ func Generate(tableName string) (string, error) {
 		ftype = getDataType(ftype)
 		fieldTypes = append(fieldTypes, ftype)
 	}
-	structString := "type " + strings.Title(tableName) + " struct{\n"
+	structString := "type " + tableName + " struct{\n"
 	for i, v := range fields {
 		structString += "\t" + v + "\t" + fieldTypes[i] + "\t\t`field:\"" + unCamelCase(v) + "\"`" + "\n"
 	}
